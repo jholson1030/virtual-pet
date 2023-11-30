c83edce120187b3eb323a21c8394e9ef20b92b1f
John Olson <jholson1030@gmail.com>
2023-11-29 19:29:17 -0600

    Declare basic variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const petScreen = document.querySelector('.pet-screen');
+let pet = document.querySelector('sprite');
+const petName = prompt("What is your pet's name?");
+let happiness = 100;
+let fullness = 100;
+let cleanliness = 100;
+
